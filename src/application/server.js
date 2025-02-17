1f174969432a30e321f5df175b97e880d9ab5ba7
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-02-18 00:15:04 +0700

    SETTING CORS

--- src/application/server.js
+++ src/application/server.js
@@ -12,30 +12,10 @@ const errorMiddleware = require("../middlewares/errorMiddleware.js");
 
 const web = express();
 web.use(express.json());
-web.use((req, res, next) => {
-  // Izinkan origin tertentu (http://localhost:5173)
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-
-  // Izinkan metode HTTP tertentu
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-
-  // Izinkan header tertentu (misalnya, Authorization)
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  // Izinkan pengiriman kredensial (cookies, auth headers)
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  // Tangani preflight request (OPTIONS)
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204); // No Content
-  }
-
-  next();
-});
 web.use(
   cors({
     origin: ["http://localhost:5173"], // Izinkan permintaan dari origin ini
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], //
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], //
   })
 );
 
