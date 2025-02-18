67a4a1f00fa55673b11610c86889a791b2bdea7f
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-02-18 14:31:54 +0700

    UPDATE

--- src/application/server.js
+++ src/application/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 require("../config/dbConnect.js");
 
-const { initSocket } = require("../config/socket.js");
 const https = require("https");
 const cors = require("cors");
 const express = require("express");
@@ -9,6 +8,7 @@ const path = require("path");
 const publicRouter = require("../routes/publicApi.js");
 const privateRouter = require("../routes/api.js");
 const errorMiddleware = require("../middlewares/errorMiddleware.js");
+const { initSocket } = require("../config/socket.js");
 
 const web = express();
 const server = https.createServer(web);
@@ -16,14 +16,16 @@ initSocket(server);
 
 web.use(
   cors({
-    origin: "https://natask.vercel.app",
+    origin: ["http://localhost:3000", "https://natask.vercel.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
+web.get("/", (req, res) => {
+  res.send("CORS sudah diatur!");
+});
 web.use(express.json());
 web.use("/files", express.static(path.join(__dirname, "../../files")));
-
 web.use(publicRouter);
 web.use(privateRouter);
 web.use(errorMiddleware);
