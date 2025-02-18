399fc072ecf565644693aded10da3783fe283863
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-02-18 14:02:48 +0700

    UPDATE

--- src/application/server.js
+++ src/application/server.js
@@ -18,20 +18,8 @@ web.use(
   cors({
     origin: "https://natask.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
   })
 );
-
-web.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.sendStatus(200);
-});
 
 web.use(express.json());
 web.use("/files", express.static(path.join(__dirname, "../../files")));
